75965d794bd0013bb613d97eaca99be998149a69
Shubham <ssp671995@gmail.com>
2022-05-22 23:48:50 +0530

    Added the simple address book in the program.

---
+++ src/main/java/com/bridgelabz/addressbook/model/AddressBook.java
@@ -0,0 +1,89 @@
+package com.bridgelabz.addressbook.model;
+
+public class AddressBook {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Long phoneNumber;
+    private String city;
+    private String state;
+    private Integer zip;
+
+
+    public AddressBook(Integer id, String firstName, String lastName, String email, Long phoneNumber, String city, String state, Integer zip) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getZip() {
+        return zip;
+    }
+
+    public void setZip(Integer zip) {
+        this.zip = zip;
+    }
+}
