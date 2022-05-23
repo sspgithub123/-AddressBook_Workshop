aae7923c6de880a33d4c748a2ba3690b25e2d0f0
Shubham <ssp671995@gmail.com>
2022-05-23 15:46:38 +0530

    Added the introducing DTO and Model to the AddressBook App in the program.

---
+++ src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
@@ -0,0 +1,67 @@
+package com.bridgelabz.addressbook.dto;
+
+public class AddressBookDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Long phoneNumber;
+    private String city;
+    private String state;
+    private Integer zip;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getZip() {
+        return zip;
+    }
+
+    public void setZip(Integer zip) {
+        this.zip = zip;
+    }
+}
